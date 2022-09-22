41eaa3566b3a456ffff1660c80d719f9a9ea95fb
Daniel Gómez <Daniel.Gomez@usfoods.com Daniel.Gomez@usfoods.com daniel.gomez@usfoods.com>
2022-09-21 19:35:43 -0500

    configuration typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 55000,
+      username: 'postgres',
+      password: 'postgrespw',
+      database: 'task-management',
+      autoLoadEntities: true,
+      synchronize: true
+    }),
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
