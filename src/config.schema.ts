e28987991ba5cbf23bc6b5493bc37b0c0c69d84c
Daniel Gómez <Daniel.Gomez@usfoods.com Daniel.Gomez@usfoods.com daniel.gomez@usfoods.com>
2022-10-02 15:34:54 -0500

    Implement ConfigSchema with Hapi/joi

---
+++ src/config.schema.ts
@@ -0,0 +1,11 @@
+import * as Joi from '@hapi/joi';
+
+export const configValidationSchema = Joi.object({
+  STAGE: Joi.string().required(),
+  DB_HOST: Joi.string().required(),
+  DB_PORT: Joi.number().default(55000).required(),
+  DB_USERNAME: Joi.string().required(),
+  DB_PASSWORD: Joi.string().required(),
+  DB_DATABASE: Joi.string().required(),
+  JWT_SECRET_KEY: Joi.string().required()
+});
