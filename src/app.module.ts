ad384c391e1b3ace28e31efdb447c1f615efd8eb
Daniel Gómez <Daniel.Gomez@usfoods.com Daniel.Gomez@usfoods.com daniel.gomez@usfoods.com>
2022-10-02 15:34:30 -0500

    Implement env for DB with ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,29 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 
 @Module({
   imports: [
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 55000,
-      username: 'postgres',
-      password: 'postgrespw',
-      database: 'task-management',
-      autoLoadEntities: true,
-      synchronize: true
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        autoLoadEntities: true,
+        synchronize: true
+      })
     }),
     AuthModule
   ],
